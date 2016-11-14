862f00397ba68c025bc4501d65d8cd16507de8d1
joongs <rlatpwnd8314@naver.com>
2016-11-14 21:18:20 +0900

    Create Main.java

---
+++ boo1_yellow/SumFrom1To100/SumFrom1To100/Main.java
@@ -0,0 +1,29 @@
+//Main.java
+/**********************************************
+ * 파일명 : Main.java
+ * 기능 : 1부터 100까지 숫자를 세고 합을 구하고 합을 출력한다.
+ * 메소드 : main
+ * 출력 : 합
+ * 입력 : 없음
+ * 작성자 : Joey
+ * 작성일자 : 2016/11/14
+***********************************************/
+public class Main {
+	public static void main(String[] argc) {
+		final int MAXIMUM = 100;
+		final int MINIMUM = 1;
+		int sum = 0;
+		int number = MINIMUM;
+		
+		//1. 100보다 작거나 같은 동안 반복한다.
+		while(number <= MAXIMUM) {			
+			// 1.1. 합을 구한다.
+			sum += number;
+			// 1.2. 숫자를 센다.
+			number++;
+		}
+		//2. 합을 출력한다.
+		System.out.printf("%d", sum);
+		// 1.3. 끝내다.
+	}
+}
