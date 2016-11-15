571761daae5ef0c75519dff04449a46eef8f1970
joongs <rlatpwnd8314@naver.com>
2016-11-16 00:09:03 +0900

    Update Evaluate.c

--- boo1_yellow/CountEvenNumberAndOddNumber/Evaluate/Evaluate/Evaluate.c
+++ boo1_yellow/CountEvenNumberAndOddNumber/Evaluate/Evaluate/Evaluate.c
@@ -50,3 +50,56 @@ int main(int argc, char* argv[]){
 	//3. 끝내다
 	return 0;
 }
+
+///////////////////////////////////////////////////////////////////////
+
+//Evaluate.c
+/**************************************************************
+파일명 : Evaluate.c
+기능 : 입력하는 만큼 수들을 입력받고, 나머지를 구하고 나머지를 기준으로 평가하고, 짝수의 개수와 홀수의 개수를 세고, 짝수의 개수와 홀수의 개수를 출력한다.
+함수명칭 : main
+출력 : 짝수의 개수, 홀수의 개수
+입력 : 수들
+작성자 : 김세중
+작성일자 : 2016/11/15
+원장님 피드백
+1. 나머지 연산자(%)사용
+**************************************************************/
+//전처리기
+#include <stdio.h>
+#define DIVISOR 2
+#define EVENREMAINDER 0
+
+//함수의 선언
+int main(int argc, char* argv[]);
+
+//함수의 정의
+int main(int argc, char* argv[]){
+	//자동변수 선언 및 정의
+	int evenCount = 0;
+	int oddCount = 0;
+	int number;
+	int remainder;
+
+	// 1.1. 수를 입력받는다.
+	scanf("%d", &number);
+	//1. 파일의 끝이 아닌동안 반복한다.
+	while (!feof(stdin)) {		
+		remainder = number;
+		remainder %= DIVISOR;
+		// 1.2. 짝수이면, 짝수의 개수를 세다.
+		if (remainder == EVENREMAINDER) {
+			evenCount++;
+		}
+		// 1.3. 홀수이면, 홀수의 개수를 세다.
+		else {
+			oddCount++;
+		}		
+		// 1.1. 수를 입력받는다.
+		scanf("%d", &number);
+	}
+	//2. 짝수의 개수, 홀수의 개수를 출력한다.
+	printf("%d %d\n", evenCount, oddCount);
+	//3. 끝내다
+	return 0;
+}
