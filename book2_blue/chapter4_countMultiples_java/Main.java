8ace52cfa77042812fe458491b265d7c32c52a78
joongs <rlatpwnd8314@naver.com>
2016-11-28 21:50:27 +0900

    Create Main.java

---
+++ book2_blue/chapter4_countMultiples_java/Main.java
@@ -0,0 +1,41 @@
+//Main.java
+/*
+ * 파일    명칭 : Main.java
+ * 메소드 명칭 : main
+ * 기          능 : 1부터 100000까지 수들에서 개수를 세고, 7의 배수를 구하고, 개수와 7의 배수를 출력한다.
+ *          그리고 마지막으로 전체 개수를 출력한다.
+ * 작   성   자 : Joey
+ * 작성   일자 : 2016/11/28
+ */
+
+// 패키지 선언
+package countmultiples;
+
+//클래스 선언
+public class Main {
+	//메소드 선언
+	public static void main(String[] argc) {
+		//상수 변수 선언
+		final int MAX = 100000;
+		final int MULTIPLE = 7;
+		
+		//자동 변수 선언
+		int count = 0;
+		int number = 0;
+		
+		//1. 제목을 출력한다.
+		System.out.println("개수\t\t\t7의 배수");
+		// 2.3. 7의 배수를 구한다.
+		//2. 수가 MAX보다 작거나 같은동안 반복한다.
+		for(number = 7; number <= MAX; number = number + MULTIPLE) {
+			// 2.1. 개수를 센다.
+			count++;
+			// 2.2. 개수와 7의 배수를 출력한다.
+			System.out.printf("%d\t\t\t%d\n",  count, number);
+			// 2.3. 7의 배수를 구한다.
+		}
+		//3. 개수를 출력한다.
+		System.out.println("7의 배수 개수 " + count);
+		//4. 끝내다.
+	}
+}
